7664327ee8ea45827d5b5e26df46e2f00747b38c
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-18 13:37:21 -0700

    Mobile and web working

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const cors = require('cors');
 
 const resolvers = require('./resolvers');
+
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  credentials: true // <-- REQUIRED backend setting
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -15,5 +21,7 @@ const server = new GraphQLServer({
     })
   })
 });
+
+server.use(cors());
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
--- api/src/resolvers/queries.js
+++ api/src/resolvers/queries.js
@@ -1,7 +1,5 @@
-const getGroupById = (root, { groupId }, ctx, info) => {
-  console.log({ groupId });
-  return ctx.db.query.group({ where: { id: groupId } }, info);
-};
+const getGroupById = (root, { groupId }, ctx, info) =>
+  ctx.db.query.group({ where: { id: groupId } }, info);
 
 const getAllGroups = (root, args, ctx, info) => ctx.db.query.groups({}, info);
 
